bd547b125d76b717293384d1f3c0049e1ff49aeb
Ivan Levkivskyi <levkivskyi@gmail.com>
2017-02-03 03:39:52 +0100

    Clean-up and combining two chromosomes to reproduce PLINK

--- src/make_asd.py
+++ src/make_asd.py
@@ -6,9 +6,6 @@ Created on Fri Jun 24 15:29:37 2016
 """
 
 import numpy as np
-import time
-
-start = time.time()
 
 N = 294
 sites = 24112
@@ -18,25 +15,20 @@ name = '../test/bla.tped'
 def fun(a, b):
     filt = np.logical_and(a, b)
     dst = filt * np.abs(a-b)
-    norm = np.count_nonzero(filt)
-    return np.sum(dst)/norm
+    return np.sum(dst)/np.sum(filt)
 
 with open(name) as f:
     data = f.read().splitlines()
 
 data = np.array([np.fromstring(l[-cut:], sep=' ', dtype='int8') for l in data])
 
-finish = time.time()
-print(finish-start)
+data = data[:,::2] + data[:,1::2]
 
-delta = np.zeros((2*N, 2*N))
+delta = np.zeros((N, N))
 
-for i in range(2*N):
+for i in range(N):
     print(i)
-    for j in range(i+1, 2*N):
+    for j in range(i+1, N):
         delta[i,j] = fun(data[:,i], data[:,j])
 
 delta = delta + delta.T
-
-finish = time.time()
-print(finish-start)
