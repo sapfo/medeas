c6b5536d8cb8b9011ecef0e9efaa10efa7b60e19
Ivan Levkivskyi <levkivskyi@gmail.com>
2017-02-28 18:20:34 +0100

    First attempt of Windows support

--- src/make_asd.py
+++ src/make_asd.py
@@ -8,44 +8,14 @@ Created on Fri Jun 24 15:29:37 2016
 import numpy as np
 from multiprocessing import Process, Queue, cpu_count
 
-# ---------- constants
-
 N = 294
-sites = 24112
-cut = N*4
-name = '../test/bla.tped'
-
-NPROC = cpu_count()
-
-# this should be large to avoid overhead of spawning new processes
-# or we need to reuse them somehow
-MAXSIZE = 100*2**20 # 100 MB
-
-# ---------- distance functions
-
-def test_func(dst):
-    if dst > 1:
-        return dst**2
-    else:
-        return 0
-
-dist_func = np.vectorize(test_func)
-dist_func = np.abs
-dist_func = np.square
-
-# ---------- global data
-
-tot_dists = np.zeros((N, N))
-tot_norms = np.zeros((N, N))
-tasks = Queue()
-results = Queue()
 
 def dist_and_norm(a, b, dist_func):
     filt = np.logical_and(a, b)
     dst = filt * dist_func(a-b)
     return np.sum(dst), np.sum(filt)
 
-def compute(i):
+def compute(i, data, dist_func):
     dists = []
     norms = []
     for j in range(i+1, N):
@@ -54,14 +24,14 @@ def compute(i):
         norms.append(norm)
     return dists, norms
 
-def work(tasks, results):
+def work(tasks, results, data, dist_func):
     while True:
         i = tasks.get()
         if i < 0:
             return
-        results.put((i, compute(i)))
+        results.put((i, compute(i, data, dist_func)))
 
-def process():
+def process(data: object, dist_func) -> object:
     dists = np.zeros((N, N))
     norms = np.zeros((N, N))
 
@@ -70,7 +40,7 @@ def process():
     for _ in range(NPROC):
         tasks.put(-1)
 
-    procs = [Process(target=work, args=(tasks, results))
+    procs = [Process(target=work, args=(tasks, results, data, dist_func))
              for _ in range(NPROC)]
     for proc in procs:
         proc.start()
@@ -87,24 +57,62 @@ def process():
 
     return dists, norms
 
-# ---------- main part
+if __name__ == '__main__':
 
-with open(name) as f:
-    while True:
-        data_lines = f.readlines(MAXSIZE)
-        if not data_lines:
-            break
-        data = np.array([np.fromstring(line[-cut:-1], sep=' ', dtype='int8')
-                         for line in data_lines])
-        data = data[:, ::2] + data[:, 1::2]
-        data = data.T.copy()
-        dists, norms = process() # TODO: avoid being beaten :)
-        tot_dists += dists
-        tot_norms += norms
+    # On Windows, processes execute the wholw file before forking
+    # therefore we protect this code with if __name__ == '__main__'
+    # Need to think how to avoid copying ``data`` on forking.
+    # On POSIX everything is already fine because of "copy-on-write"
 
-delta = (tot_dists + 1e-16)/(tot_norms + 1e-8)  # TODO: do this cleanly
-for i in range(N):
-    delta[i, i] = 0
+    def test_func(dst):
+        if dst > 1:
+            return dst**2
+        else:
+            return 0
 
-delta = delta + delta.T
-delta = np.sqrt(delta)
+    dist_func = np.vectorize(test_func)
+    dist_func = np.abs
+    #dist_func = np.square
+
+    # ---------- constants
+
+    N = 294
+    sites = 24112
+    cut = N*4
+    name = 'C:\\Users\\levkivskyi\\PycharmProjects\\medeas\\test\\bla.tped'
+
+    NPROC = cpu_count()
+
+    # this should be large to avoid overhead of spawning new processes
+    # or we need to reuse them somehow
+    MAXSIZE = 100*2**20 # 100 MB
+
+    # ---------- global data
+
+    tot_dists = np.zeros((N, N))
+    tot_norms = np.zeros((N, N))
+    tasks = Queue()
+    results = Queue()
+
+    with open(name) as f:
+        while True:
+            data_lines = f.readlines(MAXSIZE)
+            if not data_lines:
+                break
+            data = np.array([np.fromstring(line[-cut:-1], sep=' ', dtype='int8')
+                             for line in data_lines])
+            data = data[:, ::2] + data[:, 1::2]
+            data = data.T.copy()
+            dists, norms = process(data, dist_func) # TODO: avoid being beaten :)
+            tot_dists += dists
+            tot_norms += norms
+
+    delta = (tot_dists + 1e-16)/(tot_norms + 1e-8)  # TODO: do this cleanly
+    for i in range(N):
+        delta[i, i] = 0
+
+    delta = delta + delta.T
+    delta = np.sqrt(delta)
+
+    print('woohoo!')
+
