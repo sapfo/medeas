abb4afb30e7057b8de41c6f7152d1a4a42be67b4
sapfo <anna-sapfo.malaspinas@iee.unibe.ch>
2018-01-02 17:51:16 +0100

    added some comments on simulate.py and README.rst

--- README.rst
+++ README.rst
@@ -1,3 +1,23 @@
 ======
 medeas
 ======
+listing of directories, 02.01.18
+================================
+main.py: main entry point
+options.py
+processing
+__pycache__
+README.rst
+simulate.py: script that runs many other scripts
+src
+test
+
+tracy.py: not using yet, it gives pvalues as a  function of sigma (for now using a fixed pvalue so there is a single value of the stats)
+
+transcode.py: converts a filtered output of scrm (filtered for numbers only) to the format that medeas use and saves the seed to a hardcoded filename: _tmp_seed,txt
+arguments: folder, whereiscrm, splittime /currently works with two pops, one D)
+
+within transcode.py:
+create fake_labs because medeas needs it
+
+for now, n1, n2, theta: hard coded.
--- simulate.py
+++ simulate.py
@@ -11,7 +11,7 @@ import sys
 import os
 import numpy as np
 
-folder = sys.argv[1]
+folder = sys.argv[1] # folder should exist. its currently created by job_sim.sh
 
 TWO_POPS = True
 FNAME = 'res_sim.txt'
@@ -20,6 +20,7 @@ def run_simulation_two_pops(n1: int, n2: int, L: int, theta: float, D: float):
     scrm_exec = sys.argv[2]
     # n1 is always an outgroup
 
+    # create fake labels because medeas needs that
     with open(os.path.join(folder, 'fake_labs.txt'), 'w') as f:
         for i in range(n1+n2):
             if i < n1:
@@ -28,6 +29,7 @@ def run_simulation_two_pops(n1: int, n2: int, L: int, theta: float, D: float):
                 pop = 'BRI'
             f.write(f'{pop}  {pop}{i}\n')
 
+    # run scrm  for simulations
     scrm_command = f'{scrm_exec} {n1+n2} {L} -t {theta} -I 2 {n1} {n2} -ej {D} 1 2 --print-model -l -1 -L'
     with open(os.path.join(folder, 'scrm_command.txt'), 'w') as f:
         f.write(scrm_command)
@@ -37,6 +39,7 @@ def run_simulation_two_pops(n1: int, n2: int, L: int, theta: float, D: float):
     output = grep.communicate()
     data = output[0].decode('utf-8')
 
+    # get the data from scrm, incl seed
     with open('_tmp_out.txt', 'w') as f:
         f.write(data)
 
@@ -44,12 +47,24 @@ def run_simulation_two_pops(n1: int, n2: int, L: int, theta: float, D: float):
                   stdout=sys.stdout)
     trans.communicate()
 
+    #read and write the seed
     with open('_tmp_seed.txt', 'r') as f:
         seed = f.read().strip()
     with open(os.path.join(folder, FNAME), 'a') as f:
-        f.write(f'{seed} {L} {2*D}')
+        f.write(f'{seed} {L} {2*D}') #RESCALE THE SPLIT TIME
 
-    medeas = Popen(['time', 'python', 'medeas/main.py',
+    #run medeas, that is main.py, on the simulated data
+    # simulation flag is hard coded for now: SIMULATION = True, needs changing
+    #arguments for medeas:
+    #argv[1] : ancestry pattern (painting) -- never read, SIMULATION = True
+    #argv[2] : SNP data -- never read, SIMULATION = True
+    #argv[3] : labels file -- neer read, SIMULATION = True
+    #argv[4:-4]: flags, -asd: calculate the matrix, -analyze: run all analyses
+    #argv[-4]: true nssites
+    #argv[-3]: true split time
+    #argv[-2]: folder where the simulated data lives
+    #argv[-1]: filename where to store the summary of the simulation
+    medeas = Popen(['time','python','medeas/main.py',
                     'TEMP',
                     'TEMP',
                     'TEMP',
@@ -57,17 +72,18 @@ def run_simulation_two_pops(n1: int, n2: int, L: int, theta: float, D: float):
     medeas.communicate()
 
 if TWO_POPS:
-
+    # run for a range lf nssites
     Ls = [10**i for i in range(2, 5)]
     with open(os.path.join(folder, FNAME), 'a') as f:
         f.write(f'Seed L D D_inf D_std K_inf T L_inf\n')
 
+    # medeas is run on simulation mode so it will append the inferred values
     D = float(sys.argv[3])
     for L in Ls:
         run_simulation_two_pops(50, 150, int(L), 1, D)
 
 def run_simulation_three_pops(n1: int, n2: int, n3: int, L: int, theta: float, D: float, D1: float):
-
+    # DOES NOT WORK YET
     # n1 is always an outgroup
 
     with open('fake_labs.txt', 'w') as f:
@@ -106,7 +122,6 @@ def run_simulation_three_pops(n1: int, n2: int, n3: int, L: int, theta: float, D
     medeas.communicate()
 
 if not TWO_POPS:
-
     Ls3 = [100, 300, 1000, 3000, 10000, 30000]
     for L in Ls3:
         for _ in range(10):
