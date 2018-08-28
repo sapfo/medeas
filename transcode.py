54935cbdb05fcf344899455c77f3f796af416655
Frédéric Michaud <frederic.a.michaud@gmail.com>
2018-08-28 09:42:00 +0200

    Refactoring name of transcode.py to make the algorithm more transparent.

--- transcode.py
+++ transcode.py
@@ -6,10 +6,13 @@ Created on Tue Sep 19 12:11:31 2017
 @author: ivan
 """
 
+# According to scrm, and we follow the convention here, a locus is
+# a physical region which  might contain various snp.
+
 import sys
 
-n = int(sys.argv[4])
-print(f'n = {n}')
+nb_individual = int(sys.argv[4])
+print(f'n = {nb_individual}')
 
 with open(sys.argv[1]) as f:
     data = f.readlines()
@@ -20,19 +23,19 @@ with open(sys.argv[3], 'w') as f:
     f.write(seed)
 data = [d for d in data[1:] if d]
 
-tot_blocks = len(data)//n
-blocks = [None] * tot_blocks
-print(f'Total blocks: {tot_blocks}')
+nb_loci = len(data) // nb_individual
+loci = [None] * nb_loci
+print(f'number of loci: {nb_loci}')
 
-for i in range(tot_blocks):
-    blocks[i] = data[i*n:(i+1)*n]
+for i in range(nb_loci):
+    loci[i] = data[i * nb_individual:(i + 1) * nb_individual]
 
 with open(sys.argv[2], 'w') as f:
-    for block in blocks:
-        for snp in range(len(block[0])):
-            for ind in range(n):
-                s = block[ind][snp]
+    for locus in loci:
+        for snp in range(len(locus[0])):
+            for individual in range(nb_individual):
+                s = locus[individual][snp]
                 f.write(str(int(s) + 1))
-                if ind < n - 1:
+                if individual < nb_individual - 1:
                     f.write(' ')
             f.write('\n')
