f436f4a61d76642193bba0f819080b53c6e342cb
Frédéric Michaud <frederic.a.michaud@gmail.com>
2018-11-09 11:40:37 +0100

    Changing the number of plot (which axis) that are draw with MDS/PCA

--- src/simulation.py
+++ src/simulation.py
@@ -141,29 +141,29 @@ class SimulationInfo(object):
         markers = [".", "v", "*", "+", "x", "2", "p", "^", "s"]
         #TODO: find a smart way to display the data with different type of markers
         markers = markers*(1+len(np.unique(label_given))//len(markers))
-        for p in range(len(np.unique(labels_inferred))+2):
-            for q in range(p+1, len(np.unique(labels_inferred))+2):
-                fig, ax = plt.subplots(figsize=(15, 10))
-                for population_index, population_name in enumerate(np.unique(label_given)):
-                    position_population = np.where(population_name == label_given)
-                    index_colors = labels_inferred[position_population]
-                    color_value = [colors[index_color] for index_color in index_colors]
-                    markers_value = markers[population_index]
-                    ax.scatter(arr.T[p,position_population].ravel(), arr.T[q, position_population].ravel(), c=color_value, marker=markers_value, s=100)
-                plt.legend(np.unique(label_given))
-                leg = ax.get_legend()
-                for point in leg.legendHandles:
-                    point.set_color('black')
-                dir_plot = os.path.join(self.output_folder, "mds_plot")
-                if not os.path.isdir(dir_plot):
-                    os.mkdir(dir_plot)
-                markers_shape = [mlines.Line2D([], [], color='black', marker=marker_shape, linestyle='None') for marker_shape in markers]
-                markers_color = [mlines.Line2D([], [], color=marker_color, marker="o", linestyle='None') for marker_color in colors]
-                legend1 = plt.legend(markers_shape, np.unique(label_given) , loc=7,title="True population")
-                legend2 = plt.legend(markers_color, np.unique(labels_inferred), loc=9,title="Inferred population")
-                ax.add_artist(legend1)
-                ax.add_artist(legend2)
-                ax.set_xlabel(f'PC. {p+1}')
-                ax.set_ylabel(f'PC. {q+1}')
-                fig.savefig(os.path.join(dir_plot, f'mds_axis{p+1}_{q+1}.pdf'))
-                plt.close()
+        for p in range(0,self.K ,2):
+            q = p + 1
+            fig, ax = plt.subplots(figsize=(15, 10))
+            for population_index, population_name in enumerate(np.unique(label_given)):
+                position_population = np.where(population_name == label_given)
+                index_colors = labels_inferred[position_population]
+                color_value = [colors[index_color] for index_color in index_colors]
+                markers_value = markers[population_index]
+                ax.scatter(arr.T[p,position_population].ravel(), arr.T[q, position_population].ravel(), c=color_value, marker=markers_value, s=100)
+            plt.legend(np.unique(label_given))
+            leg = ax.get_legend()
+            for point in leg.legendHandles:
+                point.set_color('black')
+            dir_plot = os.path.join(self.output_folder, "mds_plot")
+            if not os.path.isdir(dir_plot):
+                os.mkdir(dir_plot)
+            markers_shape = [mlines.Line2D([], [], color='black', marker=marker_shape, linestyle='None') for marker_shape in markers]
+            markers_color = [mlines.Line2D([], [], color=marker_color, marker="o", linestyle='None') for marker_color in colors]
+            legend1 = plt.legend(markers_shape, np.unique(label_given) , loc=7,title="True population")
+            legend2 = plt.legend(markers_color, np.unique(labels_inferred), loc=9,title="Inferred population")
+            ax.add_artist(legend1)
+            ax.add_artist(legend2)
+            ax.set_xlabel(f'PC. {p+1}')
+            ax.set_ylabel(f'PC. {q+1}')
+            fig.savefig(os.path.join(dir_plot, f'mds_axis{p+1}_{q+1}.pdf'))
+            plt.close()
